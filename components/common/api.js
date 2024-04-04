bc0c64b14a48cff5c5cd785c801f4fe695f4f54f
RafayRasheed <rafayrasheed777.rr@gmail.com>
2024-04-05 01:48:01 +0500

    FEAG

---
+++ components/common/api.js
@@ -0,0 +1,12 @@
+const local = '127.0.0.1:3000'
+const live = '127.0.0.1:3000'
+
+const api = local
+
+export const allUsers = `${api}/users`
+export const signinAPI = `${api}/users/signin`
+export const saveUserAPI = `${api}/users/saveUser`
+export const sigupAPI = `${api}/users/signup`
+export const sendEmailAPI = `${api}/users/sendEmail`
+export const updateProfileAPI = `${api}/users/updateUser`
+export const updateProfileImageAPI = `${api}/users/updateImage`
