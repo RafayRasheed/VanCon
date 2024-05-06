4bb47580ffd6cc75f5302fcf14eb56ba5fc6d6c4
Shahzaib-Naveed <rafayrasheed777.rr@gmail.com>
2024-05-06 09:11:29 +0500

    dshydrtu

--- components/common/api.js
+++ components/common/api.js
@@ -1,12 +1,13 @@
-const local = 'http://192.168.1.100:3000'
-const live = '127.0.0.1:3000'
+const local = 'http://192.168.1.100:3000';
+const localOffice = 'http://172.16.1.232:3000';
+const live = '127.0.0.1:3000';
 
-const api = local
+const api = localOffice;
 
-export const allUsersAPI = `${api}/users`
-export const signinAPI = `${api}/users/signin`
-export const saveUserAPI = `${api}/users/saveUser`
-export const sigupAPI = `${api}/users/signup`
-export const sendEmailAPI = `${api}/users/sendEmail`
-export const updateProfileAPI = `${api}/users/updateUser`
-export const updateProfileImageAPI = `${api}/users/updateImage`
+export const allUsersAPI = `${api}/users`;
+export const signinAPI = `${api}/users/signin`;
+export const saveUserAPI = `${api}/users/saveUser`;
+export const sigupAPI = `${api}/users/signup`;
+export const sendEmailAPI = `${api}/users/sendEmail`;
+export const updateProfileAPI = `${api}/users/updateUser`;
+export const updateProfileImageAPI = `${api}/users/updateImage`;
